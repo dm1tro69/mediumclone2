211e2e2d03e36e35c9725728e72107c28651606d
Dmytro Voronov <dimolg22@gmail.com>
2023-01-06 18:36:39 +0200

    add login and registaration

---
+++ src/user/types/userResponse.interface.ts
@@ -0,0 +1,6 @@
+import { UserEntity } from "@app/user/user.entity";
+import { UserType } from "@app/user/types/user.type";
+
+export interface UserResponseInterface {
+   user: UserType & {token:string}
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,53 @@
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { CreateUserDto } from "@app/user/dto/createUser.dto";
+import { UserEntity } from "@app/user/user.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import {sign} from 'jsonwebtoken'
+import { JWT_SECRET } from "@app/config";
+import { UserResponseInterface } from "@app/user/types/userResponse.interface";
+import { LoginUserDto } from "@app/user/dto/loginUser.dto";
+import {compare} from 'bcrypt'
+
+
+@Injectable()
+export class UserService {
+
+  constructor(@InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const userByEmail = await this.userRepository.findOne({where: {email: createUserDto.email}})
+    const userByUsername = await this.userRepository.findOne({where: {username: createUserDto.username}})
+    if (userByEmail || userByUsername){
+      throw new HttpException('Email or username are taken', HttpStatus.UNPROCESSABLE_ENTITY)
+    }
+    const newUser = new UserEntity()
+    Object.assign(newUser, createUserDto)
+     return await this.userRepository.save(newUser)
+  }
+  generateJwt(user: UserEntity): string{
+      return sign({id: user.id, email: user.email, username: user.username}, JWT_SECRET)
+  }
+
+  buildUserResponse(user: UserEntity): UserResponseInterface{
+      return {
+        user: {
+          ...user,
+          token: this.generateJwt(user)
+        }
+      }
+  }
+  async login(loginUserDto: LoginUserDto): Promise<UserEntity>{
+       const user = await this.userRepository.findOne({where:{email: loginUserDto.email}, select: ['id', 'username', 'email', 'bio', 'image', 'password']})
+    if (!user){
+      throw new HttpException('Credential are not valid', HttpStatus.UNPROCESSABLE_ENTITY)
+    }
+    const isPasswordCorrect = await compare(loginUserDto.password, user.password)
+    if (!isPasswordCorrect){
+      throw new HttpException('Credential are not valid', HttpStatus.UNPROCESSABLE_ENTITY)
+    }
+    delete user.password
+    return user
+  }
+
+}
